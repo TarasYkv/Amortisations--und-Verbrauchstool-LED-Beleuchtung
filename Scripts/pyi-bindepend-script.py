036d2cd08089fda086953c64745ebcc7b88f7ef9
Taras.Yuzkiv <60103303+TarasYkv@users.noreply.github.com>
2020-11-25 13:39:51 +0100

    venv script projekte

---
+++ Scripts/pyi-bindepend-script.py
@@ -0,0 +1,12 @@
+#!C:\taras\neu2\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pyinstaller==3.6','console_scripts','pyi-bindepend'
+__requires__ = 'pyinstaller==3.6'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pyinstaller==3.6', 'console_scripts', 'pyi-bindepend')()
+    )
